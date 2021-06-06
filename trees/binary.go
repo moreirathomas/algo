e059694290f7330ca4faa9ae08c91d8587441028
moreirathomas <moreirathomas97@gmail.com>
2021-06-06 23:59:19 +0200

    feat: insert nodes based on value and use values slice for new tree

--- trees/binary.go
+++ trees/binary.go
@@ -2,8 +2,6 @@ package trees
 
 import (
 	"encoding/json"
-
-	"github.com/moreirathomas/algo/util"
 )
 
 type treeNode struct {
@@ -14,53 +12,60 @@ type treeNode struct {
 	Root   *treeNode `json:"-"`
 }
 
-type child int
-
-const (
-	ChildLeft child = iota
-	ChildRight
-)
-
-// New returns a new binary tree given the root node's value.
-func NewBinaryTree(rootVal int) *treeNode {
-	return &treeNode{Val: rootVal}
-}
-
-// New returns a new binary tree with a randomized initial value.
-func NewBinaryTreeRandomized() *treeNode {
-	return &treeNode{Val: util.RandomNonZeroDigit()}
-}
-
-// AddNode adds a new child to a node. It returns the node just added.
-func (node *treeNode) AddNode(val int, child child) *treeNode {
-	nextNode := newNode(val, node)
-	switch child {
-	case ChildLeft:
-		node.Left = nextNode
-	case ChildRight:
-		node.Right = nextNode
+// NewBinaryTree returns a binary tree. The first value passed is always the root.
+// All the others are inserted to the left or right depending on their value compared
+// to the previous node.
+func NewBinaryTree(values ...int) *treeNode {
+	root := NewNode(values[0], nil)
+	for _, v := range values[1:] {
+		node := NewNode(v, root)
+		root.InsertNode(node)
 	}
-	return nextNode
+	return root
 }
 
-// newNode returns a new node given a value and its parent.
+// InsertNode inserts the given node on the tree. A node whose value is greater
+// than the previous one is inserted to the right, and to the left otherwise.
+func (tree *treeNode) InsertNode(node *treeNode) {
+	if tree == nil {
+		panic("cannot add a node to a nil parent")
+	}
+	if node.Val < tree.Val {
+		if tree.Left == nil {
+			tree.Left = node
+		}
+		tree.Left.InsertNode(node)
+	}
+	if node.Val > tree.Val {
+		if tree.Right == nil {
+			tree.Right = node
+		}
+		tree.Right.InsertNode(node)
+	}
+}
+
+// NewNode returns a new node given a value and its parent.
 // The root node is inferred by the parent.
-func newNode(val int, parent *treeNode) *treeNode {
-	// Infer the root node. If there is no root is defined on the parent, it is the root.
-	root := parent.Root
-	if root == nil {
-		root = parent
+func NewNode(val int, parent *treeNode) *treeNode {
+	// Infer the root node: either this is the root,
+	// or the parent has no root and thus is the root, or it knows the root.
+	root := parent
+	if parent != nil && parent.Root != nil {
+		root = parent.Root
 	}
+	return &treeNode{Val: val, Parent: parent, Root: root}
+}
 
-	return &treeNode{
-		Val:    val,
-		Parent: parent,
-		Left:   nil,
-		Right:  nil,
-		Root:   root,
+// Invert inverts a tree in place.
+func (tree *treeNode) Invert() {
+	if tree != nil {
+		tree.Left, tree.Right = tree.Right, tree.Left
+		tree.Left.Invert()
+		tree.Right.Invert()
 	}
 }
 
+// Sibling returns the other node at the same level of depth from the same parent.
 func (node *treeNode) Sibling() *treeNode {
 	// eh.
 	if node.Parent == nil {
@@ -69,19 +74,6 @@ func (node *treeNode) Sibling() *treeNode {
 		return node.Parent.Right
 	} else {
 		return node
-	}
-}
-
-// Invert inverts a tree in place.
-func (tree *treeNode) Invert() {
-	invert(tree)
-}
-
-func invert(node *treeNode) {
-	if node != nil {
-		node.Left, node.Right = node.Right, node.Left
-		invert(node.Left)
-		invert(node.Right)
 	}
 }
 
--- trees/binary_test.go
+++ trees/binary_test.go
@@ -7,20 +7,83 @@ import (
 	"github.com/moreirathomas/algo/util"
 )
 
-func TestAddNode(t *testing.T) {
-	tree := trees.NewBinaryTreeRandomized()
-	child := tree.AddNode(util.RandomNonZeroDigit(), trees.ChildLeft)
+// This file uses a shared tree for all its tests.
+var valuesDepth2 = []int{4, 2, 7, 1, 3, 6, 9}
 
-	if child != tree.Left {
-		t.Error("expected child to be added to the tree")
+// It is represented as this:
+// ⁠     4
+// ⁠   /  \
+// ⁠  2    7
+// ⁠ / \  / \
+// 1  3 6  9
+
+// In its inverted form:
+// ⁠     4
+// ⁠   /  \
+// ⁠  7    2
+// ⁠ / \  / \
+// 9  6 3  1
+
+// A simpler tree with only a depth of 1.
+var valuesDepth1 = valuesDepth2[:3]
+
+func TestNewTree(t *testing.T) {
+	tree := trees.NewBinaryTree(valuesDepth2...)
+
+	badNodeValueErrorFmt := func(node string, want, got int) {
+		t.Errorf("expected tree to have correct at %s node: want %d, got %d", node, want, got)
+	}
+
+	if tree.Val != 4 {
+		badNodeValueErrorFmt("root", 4, tree.Val)
+	}
+	if tree.Left.Val != 2 {
+		badNodeValueErrorFmt("left child", 2, tree.Left.Val)
+	}
+	if tree.Left.Left.Val != 1 {
+		badNodeValueErrorFmt("left child's left child", 1, tree.Left.Left.Val)
+	}
+	if tree.Left.Right.Val != 3 {
+		badNodeValueErrorFmt("left child's right child", 3, tree.Left.Right.Val)
+	}
+	if tree.Right.Val != 7 {
+		badNodeValueErrorFmt("right child", 7, tree.Right.Val)
+	}
+	if tree.Right.Left.Val != 6 {
+		badNodeValueErrorFmt("right child's left child", 6, tree.Right.Left.Val)
+	}
+	if tree.Right.Right.Val != 9 {
+		badNodeValueErrorFmt("right child's right child", 9, tree.Right.Right.Val)
+	}
+}
+
+func TestInsertNode(t *testing.T) {
+	tree := trees.NewBinaryTree(util.RandomNonZeroDigit())
+	node := trees.NewNode(util.RandomNonZeroDigit(), tree)
+	tree.InsertNode(node)
+
+	insertionErrorFmt := func(where string) {
+		t.Errorf("expected node to be inserted to the %s: got tree %s, want tree with node %s",
+			where, tree.String(), node.String())
+	}
+
+	switch {
+	case node.Val < tree.Val:
+		if node != tree.Left {
+			insertionErrorFmt("left")
+		}
+	case node.Val > tree.Val:
+		if node != tree.Right {
+			insertionErrorFmt("right")
+		}
+	default:
+		t.Errorf("unexpected error: unable to compare %v and %v", tree, node)
 	}
 }
 
 func TestRootNode(t *testing.T) {
-	tree := trees.NewBinaryTreeRandomized()
-	child := tree.AddNode(util.RandomNonZeroDigit(), trees.ChildLeft)
-	grandChild := child.AddNode(util.RandomNonZeroDigit(), trees.ChildLeft)
-	root := grandChild.Root
+	tree := trees.NewBinaryTree(valuesDepth2...)
+	root := tree.Left.Left.Root
 
 	if root == nil || root != tree {
 		t.Errorf("expected to get root node: want %s, got %s", tree.String(), root.String())
@@ -28,9 +91,7 @@ func TestRootNode(t *testing.T) {
 }
 
 func TestSiblingNode(t *testing.T) {
-	tree := trees.NewBinaryTreeRandomized()
-	tree.AddNode(util.RandomNonZeroDigit(), trees.ChildLeft)
-	tree.AddNode(util.RandomNonZeroDigit(), trees.ChildLeft)
+	tree := trees.NewBinaryTree(valuesDepth1...)
 	sibling := tree.Left.Sibling()
 
 	if sibling != tree.Right {
@@ -40,53 +101,30 @@ func TestSiblingNode(t *testing.T) {
 
 func TestInvertTree(t *testing.T) {
 
-	// Given this tree:
-	// ⁠     4
-	// ⁠   /  \
-	// ⁠  2    7
-	// ⁠ / \  / \
-	// 1  3 6  9
-
-	// The inverted tree shoudl be:
-	// ⁠     4
-	// ⁠   /  \
-	// ⁠  7    2
-	// ⁠ / \  / \
-	// 9  6 3  1
-
-	tree := trees.NewBinaryTree(4)
-
-	leftChild := tree.AddNode(2, trees.ChildLeft)
-
-	leftChild.AddNode(1, trees.ChildLeft)
-	leftChild.AddNode(3, trees.ChildRight)
-
-	rightChild := tree.AddNode(7, trees.ChildRight)
-	rightChild.AddNode(6, trees.ChildLeft)
-	rightChild.AddNode(9, trees.ChildRight)
+	tree := trees.NewBinaryTree(valuesDepth2...)
 
 	tree.Invert()
 
-	badInversionError := func(node string, want, got int) {
+	badNodeValueErrorFmt := func(node string, want, got int) {
 		t.Errorf("expected tree to be inverted at node %s: want %d, got %d", node, want, got)
 	}
 
 	if tree.Left.Val != 7 {
-		badInversionError("left child", 7, tree.Left.Val)
+		badNodeValueErrorFmt("left child", 7, tree.Left.Val)
 	}
 	if tree.Left.Left.Val != 9 {
-		badInversionError("left child's left child", 9, tree.Left.Left.Val)
+		badNodeValueErrorFmt("left child's left child", 9, tree.Left.Left.Val)
 	}
 	if tree.Left.Right.Val != 6 {
-		badInversionError("left child's right child", 6, tree.Left.Right.Val)
+		badNodeValueErrorFmt("left child's right child", 6, tree.Left.Right.Val)
 	}
 	if tree.Right.Val != 2 {
-		badInversionError("right child", 2, tree.Right.Val)
+		badNodeValueErrorFmt("right child", 2, tree.Right.Val)
 	}
 	if tree.Right.Left.Val != 3 {
-		badInversionError("right child's left child", 3, tree.Right.Left.Val)
+		badNodeValueErrorFmt("right child's left child", 3, tree.Right.Left.Val)
 	}
 	if tree.Right.Right.Val != 1 {
-		badInversionError("right child's right child", 1, tree.Right.Right.Val)
+		badNodeValueErrorFmt("right child's right child", 1, tree.Right.Right.Val)
 	}
 }
